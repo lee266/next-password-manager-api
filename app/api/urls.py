b4745af8c2772ad8f18d104c82ab070e4d5f67c4
Lee <lee20010905@gmail.com>
2023-01-15 20:46:42 +0900

    create login and password manage

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
 from app.api import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet, basename="users")
+# router.register(r'passwords', views.PasswordManageView, basename="passwords")
+router.register(r'tasks', views.TaskViewSet, basename="tasks")
+
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('user/', views.UserView.as_view(), name='User'),
     path('message/', views.MessageView.as_view(), name='message'),
+    path('password/', views.PasswordManageView.as_view(), name="password"),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('snippets/', views.task_list),
+    path(r'test', views.TestAppView.as_view()),
 ]
--- app/api/views.py
+++ app/api/views.py
@@ -1,12 +1,61 @@
-from rest_framework import generics
-from app.models import User, Message
-from app.api.serializers import UserSerializer, MessageSerializer
+from rest_framework import generics, viewsets
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.http.response import JsonResponse
+from app.models import User, Message, PasswordManage, Task
+from app.api.serializers import UserSerializer, MessageSerializer, PasswordManageSerializer, TaskSerializer
 
 class UserView(generics.ListCreateAPIView):
   queryset = User.objects.all()
   serializer_class = UserSerializer
+  permission_classes = (AllowAny,)
 
 class MessageView(generics.ListCreateAPIView):
   queryset = Message.objects.all()
   serializer_class = MessageSerializer
+  permission_classes = (AllowAny,)
+  
+class UserViewSet(viewsets.ModelViewSet):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
+class PasswordManageView(generics.ListCreateAPIView):
+  queryset = PasswordManage.objects.all()
+  serializer_class = PasswordManageSerializer
+  permission_classes = (AllowAny,)
+
+class TaskViewSet(viewsets.ModelViewSet):
+  queryset = Task.objects.all()
+  serializer_class = TaskSerializer
+  permission_classes = (AllowAny,)
+
+
+@api_view(['GET', 'POST'])
+def task_list(request):
+  if request.method == 'GET':
+        Tasks = Task.objects.all()
+        serializer = TaskSerializer(Tasks, many=True)
+        return JsonResponse(serializer.data)
+  elif request.method == 'POST':
+      serializer = TaskSerializer(data=request.data)
+      if serializer.is_valid():
+          serializer.save()
+          return Response(serializer.data, status=status.HTTP_201_CREATED)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TestAppView(APIView):
+  permission_classes = (AllowAny,)
+  def get(self, request, **kwargs):
+    return JsonResponse({ "data": "this is get" })
+  def post(self, request, **kwargs):
+    return JsonResponse({ "data": "post" })
+  def put(self, request, **kwargs):
+    return JsonResponse({ "data": "put" })
+  def patch(self, request, **kwargs):
+    return JsonResponse({ "data": "patch" })
+  def delete(self, request, **kwargs):
+    return JsonResponse({ "data": "delete" })
+
   
