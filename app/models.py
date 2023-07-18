69e43e46ee1cc274616070073b7899df7d9c2bca
Lee <lee20010905@gmail.com>
2023-07-19 08:41:20 +0900

    Create inquiry model

--- app/models.py
+++ app/models.py
@@ -179,3 +179,27 @@ class Calendar(models.Model):
   
   def __str__(self):
     return self.title
+  
+class InquiryCategory(models.Model):
+    category_name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.category_name
+
+class Inquiry(models.Model):
+    STATUS_CHOICES = [
+        ('Open', 'Open'),
+        ('In Progress', 'In Progress'),
+        ('Closed', 'Closed'),
+    ]
+
+    inquiry_category = models.ForeignKey(InquiryCategory, on_delete=models.CASCADE)
+    inquiry_content = models.TextField(max_length=2000)
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='Open',
+    )
+    
+    def __str__(self):
+        return f"{self.inquiry_content[:50]}..."
